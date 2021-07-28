264aa44a78c786f614be9105d1d215b5fbef06ba
softhax <aashu1999@gmail.com>
2021-07-28 19:29:46 +0530

    added 7 to final

---
+++ sem6/cd/final/prog7/script.sh
@@ -0,0 +1,25 @@
+echo -e "--- Test 1 ---\n\nInput:\n"
+cat test
+echo -e "Output :\n"
+./a.out < test
+echo -e "\n\n--------------------------------------------------------\n"
+echo -e "--- Test 2 ---\n\nInput:\n"
+cat test2
+echo -e "Output :\n"
+./a.out < test2
+echo -e "\n\n--------------------------------------------------------\n"
+echo -e "--- Test 3 ---\n\nInput:\n"
+cat test3
+echo -e "Output :\n"
+./a.out < test3
+echo -e "\n\n--------------------------------------------------------\n"
+echo -e "--- Test 4 ---\n\nInput:\n"
+cat test4
+echo -e "Output :\n"
+./a.out < test4
+echo -e "\n\n--------------------------------------------------------\n"
+echo -e "--- Test 5 ---\n\nInput:\n"
+cat test5
+echo -e "Output :\n"
+./a.out < test5
+echo -e "\n\n"
