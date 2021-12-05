b92f56b1b25171f85f2589e6ac52531ac96830e8
Anurag Singh <aashu1999@gmail.com>
2021-12-05 23:58:49 +0530

    added pro5 mpi

--- sem7/padp/prog5/messagePassing.c
+++ sem7/padp/prog5/messagePassing.c
@@ -1,31 +1,31 @@
-#include<mpi.h>
-#include<string.h>
-
-#define BUFFER_SIZE 32
-
-int main(int argc,char *argv[]){
-    int MyRank,Numprocs,Destination,iproc;
-    int tag = 0;
-    int Root = 0, temp = 1;
-    char Message[BUFFER_SIZE];
-    MPI_Init(&argc,&argv);
-    MPI_Status status;
-
-    MPI_Comm_rank(MPI_COMM_WORLD,&MyRank);
-    MPI_Comm_size(MPI_COMM_WORLD,&Numprocs);
-
-    // print host name, and send message from process with rank 0 to all other processes
-    if(MyRank == 0){
-        system("hostname");
-        strcpy(Message,"Hello India");
-        for(temp=1;temp<Numprocs;temp++){
-            MPI_Send(Message,BUFFER_SIZE,MPI_CHAR,temp,tag,MPI_COMM_WORLD);
-        }
-    }
-    else{
-        system("hostname");
-        MPI_Recv(Message,BUFFER_SIZE,MPI_CHAR,Root,tag,MPI_COMM_WORLD,&status);
-        printf("\n%s in process with rank %d from Process with rank %d\n",Message,MyRank,Root);
-    }
-    MPI_Finalize();
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+#define BUFSIZE 32 
+char m[5][20]={"\nHello","\nRVCE","\nCSE","\n7th Sem","\nHPC"};
+int main(int argc,char **argv)
+{
+ char mess[32];
+ int root=0;
+ int tag =0;
+ int myRank,numProcs,temp=1;
+ MPI_Init(&argc,&argv);
+ MPI_Status status;
+ MPI_Comm_rank(MPI_COMM_WORLD,&myRank);
+ MPI_Comm_size(MPI_COMM_WORLD,&numProcs);
+ if(myRank==0)
+ {
+  for(temp=1;temp<numProcs;temp++)
+  {
+   strcpy(mess,m[temp-1]);
+   MPI_Send(&mess,BUFSIZE,MPI_CHAR,temp,tag,MPI_COMM_WORLD);
+  } 
+ }
+ else
+ {
+  MPI_Recv(&mess,BUFSIZE,MPI_CHAR,root,tag,MPI_COMM_WORLD,&status);
+  printf("\n%s is message process of rank %d sent to process of rank %d",mess,myRank,root);
+ }
+ MPI_Finalize();
+ return 0;
 }
